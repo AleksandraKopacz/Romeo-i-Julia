18ce347e778b721541b6627b42b8f16492d308a8
Aleksandra Kopacz <aleksandrakopacz4@gmail.com>
2025-02-06 02:23:48 +0100

    test

--- src/assets/js/maincharacters.js
+++ src/assets/js/maincharacters.js
@@ -12,8 +12,8 @@ const rmCtx = romeo.getContext("2d");
 loadRomeo();
 
 function loadRomeo() {
-  ROMEO.onload = () => {
-    FRAME2.onload = () => {
+  FRAME2.onload = () => {
+    ROMEO.onload = () => {
       romeo.width = FRAME2.width / 2;
       romeo.height = FRAME2.height / 2;
       rmCtx.drawImage(ROMEO, 0, 0, FRAME2.width / 2, FRAME2.height / 2);
@@ -36,12 +36,10 @@ const jlCtx = juliet.getContext("2d");
 loadJuliet();
 
 function loadJuliet() {
-  JULIET.onload = () => {
-    FRAME.onload = () => {
-      juliet.width = FRAME.width / 2;
-      juliet.height = FRAME.height / 2;
-      jlCtx.drawImage(JULIET, 0, 0, FRAME.width / 2, FRAME.height / 2);
-      jlCtx.drawImage(FRAME, 0, 0, FRAME.width / 2, FRAME.height / 2);
-    };
+  FRAME.onload = () => {
+    juliet.width = FRAME.width / 2;
+    juliet.height = FRAME.height / 2;
+    jlCtx.drawImage(JULIET, 0, 0, FRAME.width / 2, FRAME.height / 2);
+    jlCtx.drawImage(FRAME, 0, 0, FRAME.width / 2, FRAME.height / 2);
   };
 }
