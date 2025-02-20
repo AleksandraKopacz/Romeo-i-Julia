8449e2dc2e372534ce404eff1de85c0f4e25fc5d
Aleksandra Kopacz <aleksandrakopacz4@gmail.com>
2025-02-20 05:32:11 +0100

    image fix

--- src/assets/js/sidecharacters.js
+++ src/assets/js/sidecharacters.js
@@ -1,8 +1,5 @@
 //LAURENCE
 
-const FRAME3 = new Image();
-FRAME3.src = "./assets/images/frame.png";
-
 const LAURENCE = new Image();
 LAURENCE.src = "./assets/images/laurence.png";
 
@@ -12,18 +9,14 @@ const lrCtx = laurence.getContext("2d");
 loadLaurence();
 
 function loadLaurence() {
-  FRAME3.onload = () => {
-    laurence.width = FRAME3.width / 3;
-    laurence.height = FRAME3.height / 2;
-    lrCtx.drawImage(LAURENCE, 0, 75, FRAME3.width / 3, FRAME3.height / 3);
-    lrCtx.drawImage(FRAME3, 0, 75, FRAME3.width / 3, FRAME3.height / 3);
+  LAURENCE.onload = () => {
+    laurence.width = LAURENCE.width / 3;
+    laurence.height = LAURENCE.height / 2;
+    lrCtx.drawImage(LAURENCE, 0, 75, LAURENCE.width / 3, LAURENCE.height / 3);
   };
 }
 
 // MERCUTIO
-
-const FRAME6 = new Image();
-FRAME6.src = "./assets/images/frame.png";
 
 const MERCUTIO = new Image();
 MERCUTIO.src = "./assets/images/mercutio.png";
@@ -34,18 +27,14 @@ const mrCtx = mercutio.getContext("2d");
 loadMercutio();
 
 function loadMercutio() {
-  FRAME6.onload = () => {
-    mercutio.width = FRAME6.width / 3;
-    mercutio.height = FRAME6.height / 2;
-    mrCtx.drawImage(MERCUTIO, 0, 75, FRAME6.width / 3, FRAME6.height / 3);
-    mrCtx.drawImage(FRAME6, 0, 75, FRAME6.width / 3, FRAME6.height / 3);
+  MERCUTIO.onload = () => {
+    mercutio.width = MERCUTIO.width / 3;
+    mercutio.height = MERCUTIO.height / 2;
+    mrCtx.drawImage(MERCUTIO, 0, 75, MERCUTIO.width / 3, MERCUTIO.height / 3);
   };
 }
 
 // NURSE
-
-const FRAME4 = new Image();
-FRAME4.src = "./assets/images/frame.png";
 
 const NURSE = new Image();
 NURSE.src = "./assets/images/nurse.png";
@@ -56,18 +45,14 @@ const nuCtx = nurse.getContext("2d");
 loadNurse();
 
 function loadNurse() {
-  FRAME4.onload = () => {
-    nurse.width = FRAME4.width / 3;
-    nurse.height = FRAME4.height / 2;
-    nuCtx.drawImage(NURSE, 0, 75, FRAME4.width / 3, FRAME4.height / 3);
-    nuCtx.drawImage(FRAME4, 0, 75, FRAME4.width / 3, FRAME4.height / 3);
+  NURSE.onload = () => {
+    nurse.width = NURSE.width / 3;
+    nurse.height = NURSE.height / 2;
+    nuCtx.drawImage(NURSE, 0, 75, NURSE.width / 3, NURSE.height / 3);
   };
 }
 
 // TYBALT
-
-const FRAME5 = new Image();
-FRAME5.src = "./assets/images/frame.png";
 
 const TYBALT = new Image();
 TYBALT.src = "./assets/images/tybalt.png";
@@ -78,10 +63,9 @@ const tbCtx = tybalt.getContext("2d");
 loadTybalt();
 
 function loadTybalt() {
-  FRAME5.onload = () => {
-    tybalt.width = FRAME5.width / 3;
-    tybalt.height = FRAME5.height / 2;
-    tbCtx.drawImage(TYBALT, 0, 75, FRAME5.width / 3, FRAME5.height / 3);
-    tbCtx.drawImage(FRAME5, 0, 75, FRAME5.width / 3, FRAME5.height / 3);
+  TYBALT.onload = () => {
+    tybalt.width = TYBALT.width / 3;
+    tybalt.height = TYBALT.height / 2;
+    tbCtx.drawImage(TYBALT, 0, 75, TYBALT.width / 3, TYBALT.height / 3);
   };
 }
